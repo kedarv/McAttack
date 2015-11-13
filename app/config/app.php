ab6b1feb216dadd286aaa5eb305e4fe42d7d0b54
Kedar <kedar54@gmail.com>
2015-11-13 09:58:37 -0500

    add debugbar

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Barryvdh\DomPDF\ServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 
@@ -189,7 +190,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'PDF' => 'Barryvdh\DomPDF\Facade',
+		'PDF' 			  => 'Barryvdh\DomPDF\Facade',
+		'Debugbar' 		  => 'Barryvdh\Debugbar\Facade',
 
 	),
 
