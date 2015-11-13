b8af7c2aa009b26d058633a26627d26bff8f2845
Kedar <kedar54@gmail.com>
2015-11-13 09:47:42 -0500

    remove user facing methods, views

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -53,14 +53,12 @@ class PageController extends BaseController {
 		$account = Account::where('used', '!=', 1)->first()->toArray();
 		$arr = $this->getAvailableOffers($account['email']);
 		$data['email'] = $arr['email'];
-		//return $this->createPDF();
 		return View::make('home', compact('data', 'arr'));
 	}
 	public function generatePDF() {
 		$params = array("email" => Input::get('email'), "id" => Input::get('id'));
 		$coupon = $this->generateCoupon($params);
-		$data['name'] = "Hello";
-		$pdf = PDF::loadView("coupon_pdf", compact('data', 'coupon'));
+		$pdf = PDF::loadView("coupon_pdf", compact('coupon'));
 		return $pdf->download('coupon.pdf');
 	}
 	public function getAvailableOffers($email) {
@@ -204,117 +202,10 @@ class PageController extends BaseController {
 		exit();	
 	}
 	
-	public function doRegisterManual() {
-		if (Request::ajax()) {
-			$validator = Validator::make(
-		    	array(
-		        	'firstname' => Input::get('firstname'),
-		        	'lastname' => Input::get('lastname'),
-		        	'zipcode' => Input::get('zip'),
-		        	'email' => Input::get('email'),
-		        	'password' => Input::get('password'),
-		        	'confirmpassword' => Input::get('confirmpassword'),
-		    	),
-		    	array(
-		        	'firstname' => 'required',
-		        	'lastname' => 'required',
-		        	'zipcode' => 'required|numeric',
-		        	'email' => 'required|email',
-		        	'password' => 'required|min:6|max:12',
-		        	'confirmpassword' => 'required|same:password',    	
-		    	)
-			);
-			if ($validator->fails()) {
-	    		$response = array('status' => 'danger', 'text' => $validator->messages());
-	    	}
-	    	else {
-	    		$client = new \GuzzleHttp\Client();
-
-	    		$body = '{
-					"marketId": "US",
-					"application": "MOT",
-					"languageName": "en-US",
-					"platform": "iphone",
-					"userName": "'.Input::get('email').'",
-					"password": "helloworlD1",
-					"firstName": "'.Input::get('firstname').'",
-					"lastName": "'.Input::get('lastname').'",
-					"nickName": null,
-					"mobileNumber": "",
-					"emailAddress": "'.Input::get('email').'",
-					"isPrivacyPolicyAccepted": true,
-					"preferredNotification": 1,
-					"receivePromotions": true,
-					"cardItems": [],
-					"accountItems": [],
-					"zipCode": "'.Input::get('zipcode').'",
-					"optInForCommunicationChannel": false,
-					"optInForSurveys": false,
-					"optInForProgramChanges": false,
-					"optInForContests": false,
-					"optInForOtherMarketingMessages": false,
-					"notificationPreferences": {
-						"AppNotificationPreferences_OfferExpirationOption": 0,
-						"EmailNotificationPreferences_LimitedTimeOffers": false,
-						"AppNotificationPreferences_Enabled": false,
-						"EmailNotificationPreferences_EverydayOffers": false,
-						"EmailNotificationPreferences_YourOffers": false,
-						"AppNotificationPreferences_YourOffers": false,
-						"AppNotificationPreferences_PunchcardOffers": false,
-						"AppNotificationPreferences_LimitedTimeOffers": false,
-						"EmailNotificationPreferences_OfferExpirationOption": 0,
-						"EmailNotificationPreferences_Enabled": false,
-						"EmailNotificationPreferences_PunchcardOffers": false,
-						"AppNotificationPreferences_EverydayOffers": false
-					},
-					"preferredOfferCategories": [],
-					"subscribedToOffer": true,
-					"isActive": true
-				}';
-
-				$r = $client->request('POST', 'https://api.mcd.com/v3/customer/registration', [
-					'headers' => [
-		        		'Content-Type' => 'application/json',
-		        		'mcd_apikey'     => 'lvi2NZIVT42AytkqXm6E2BApBU369jpp',
-		        		'Token'      => '9efcc6dc92eb4fdf9eb6e8803deceffd',
-		        		'MarketId' => 'US'
-		    		],
-		    		'verify' => false,
-		    		'body' => $body
-				]);
-
-				$arr = json_decode((string) $r->getBody(), true);
-				if($arr['ResultCode'] == 1) {
-					$account = new Account;
-		    		$account->firstname = Input::get('firstname');
-		    		$account->lastname = Input::get('lastname');
-		    		$account->zipcode = Input::get('zip');
-		    		$account->email = Input::get('email');
-		    		$account->password = Input::get('password');
-		    		$account->save();
-	    			$response = array('status' => 'success', 'text' => 'Success');
-	    		}
-	    		else {
-	    			$response = array('status' => 'danger', 'text' => 'MCD API error');
-	    		}
-	    	}
-	    	return Response::json($response); 
-	    	exit();
-    	}
-	}
 	public function generateAccounts() {
 		$user = str_random(2);
 		$domain = str_random(2) . ".com";
 		$email = $user . "@" . $domain;
 		return $this->doRegisterAuto($email);
-	}
-
-	public function register() {
-		$data['title'] = "Register";
-		return View::make('user.register', compact('data'));
-	}
-	public function login() {
-		$data['title'] = "Login";
-		return View::make('user.login', compact('data'));
 	}
 }
